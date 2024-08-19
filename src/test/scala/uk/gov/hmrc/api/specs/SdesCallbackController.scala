720a4a43836b651a5155efda5202ffe4da556e4c
mercator <30315073+steve-edmans@users.noreply.github.com>
2024-08-19 15:32:59 +0100

    Minor fixes

--- src/test/scala/uk/gov/hmrc/api/specs/SdesCallbackController.scala
+++ src/test/scala/uk/gov/hmrc/api/specs/SdesCallbackController.scala
@@ -1,36 +1,35 @@
 package uk.gov.hmrc.api.specs
 
 import uk.gov.hmrc.api.client.HttpClient
-import uk.gov.hmrc.api.utils.InboundSdesMessage.successJsonBody
-import uk.gov.hmrc.api.utils.InboundSdesMessage.failureJsonBody
+import uk.gov.hmrc.api.utils.InboundSdesMessage.*
 
 class SdesCallbackController extends BaseSpec, HttpClient:
-    Feature("Inbound POST API handles successful SDES notification") {
-      Scenario("Central-reference-data-inbound-orchestrator endpoint works") {
-        Given("The endpoint is accessed")
-        val url = s"$host/services/crdl/callback"
-        val result = await(
-          post(
-            url,
-            successJsonBody,
-            "content-type" -> "application/json",
-          )
+  Feature("Inbound POST API handles successful SDES notification") {
+    Scenario("Central-reference-data-inbound-orchestrator endpoint works") {
+      Given("The endpoint is accessed")
+      val url    = s"$host/services/crdl/callback"
+      val result = await(
+        post(
+          url,
+          successJsonBody,
+          "content-type" -> "application/json"
         )
-        result.status shouldBe 202
-      }
-
-      Feature("Inbound POST API handles successful SDES notification") {
-        Scenario("Central-reference-data-inbound-orchestrator endpoint works") {
-          Given("The endpoint is accessed")
-          val url = s"$host/services/crdl/callback"
-          val result = await(
-            post(
-              url,
-              failureJsonBody,
-              "content-type" -> "application/json",
-            )
-          )
-          result.status shouldBe 202
-        }
-      }
+      )
+      result.status shouldBe 202
     }
+  }
+
+  Feature("Inbound POST API handles failure SDES notification") {
+    Scenario("Central-reference-data-inbound-orchestrator endpoint fails") {
+      Given("The endpoint is accessed")
+      val url    = s"$host/services/crdl/callback"
+      val result = await(
+        post(
+          url,
+          failureJsonBody,
+          "content-type" -> "application/json"
+        )
+      )
+      result.status shouldBe 202
+    }
+  }
--- src/test/scala/uk/gov/hmrc/api/utils/InboundSdesMessage.scala
+++ src/test/scala/uk/gov/hmrc/api/utils/InboundSdesMessage.scala
@@ -1,8 +1,6 @@
 package uk.gov.hmrc.api.utils
 
-import org.scalactic.Prettifier.default
-
-object InboundSdesMessage {
+object InboundSdesMessage:
   val successJsonBody: String =
     """
       |{
@@ -25,16 +23,15 @@ object InboundSdesMessage {
       |}
       |""".stripMargin
 
-    val failureJsonBody: String ="""
-        {
-                                   |"notification": "FileProcessingFailure",
-                                   |"filename": "32f2c4f7-c635-45e0-bee2-0bdd97a4a70d.zip",
-                                   |"checksumAlgorithm": "md5",
-                                   |"checksum": "894bed34007114b82fa39e05197f9eec",
-                                   |"correlationID": "32f2c4f7-c635-45e0-bee2-0bdd97a4a70d",
-                                   |"dateTime": "2020-11-09T16:48:21.659Z",
-                                   |"failureReason": "Virus Detected",
-                                   |"actionRequired": "ADDRESS-FAILURE-THEN-RETRY"
-                                   |}
-                                   |""".stripMargin
-}
+  val failureJsonBody: String =
+    """{
+      |"notification": "FileProcessingFailure",
+      |"filename": "32f2c4f7-c635-45e0-bee2-0bdd97a4a70d.zip",
+      |"checksumAlgorithm": "md5",
+      |"checksum": "894bed34007114b82fa39e05197f9eec",
+      |"correlationID": "32f2c4f7-c635-45e0-bee2-0bdd97a4a70d",
+      |"dateTime": "2020-11-09T16:48:21.659Z",
+      |"failureReason": "Virus Detected",
+      |"actionRequired": "ADDRESS-FAILURE-THEN-RETRY"
+      |}
+      |""".stripMargin
