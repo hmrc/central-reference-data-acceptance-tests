bcc7b4bd9baff2fd0f4aa61fe720230800f26d1c
Abi-Y <wenshuo.yang@accenture.com>
2024-09-12 15:54:22 +0100

    CRDL-134 Check wrapper status in Mongo after every status update

--- src/test/scala/uk/gov/hmrc/api/utils/InboundSdesMessage.scala
+++ src/test/scala/uk/gov/hmrc/api/utils/InboundSdesMessage.scala
@@ -42,13 +42,14 @@ object InboundSdesMessage:
       |}
       |""".stripMargin
 
-  val failureJsonBody: String =
-    """{
+  def failureJsonBody: String                             = failureJsonBodyFromString(UUID.randomUUID().toString)
+  def failureJsonBodyFromString(identify: String): String =
+    s"""{
       |"notification": "FileProcessingFailure",
       |"filename": "32f2c4f7-c635-45e0-bee2-0bdd97a4a70d.zip",
       |"checksumAlgorithm": "md5",
       |"checksum": "894bed34007114b82fa39e05197f9eec",
-      |"correlationID": "32f2c4f7-c635-45e0-bee2-0bdd97a4a70d",
+      |"correlationID": "$identify",
       |"dateTime": "2020-11-09T16:48:21.659Z",
       |"failureReason": "Virus Detected",
       |"actionRequired": "ADDRESS-FAILURE-THEN-RETRY"
