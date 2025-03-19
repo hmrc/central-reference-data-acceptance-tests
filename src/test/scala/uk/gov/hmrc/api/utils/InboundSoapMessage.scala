906de5f8598ae2860cc9e9c00b2547a12d828e4c
Kavitakum <79657475+Kavitakum@users.noreply.github.com>
2025-03-19 15:57:32 +0000

    CRDL-226: Updated scripts to add isAlive check scenario

--- src/test/scala/uk/gov/hmrc/api/utils/InboundSoapMessage.scala
+++ src/test/scala/uk/gov/hmrc/api/utils/InboundSoapMessage.scala
@@ -70,4 +70,36 @@ object InboundSoapMessage {
        |        </v4:ReceiveReferenceDataReqMsg>
        |      </soap:Body>
        |    </soap:Envelope>""".stripMargin
+
+  def xmlFullMessageIsAlive: String =
+    s"""<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
+         |     xmlns:v1="http://xmlns.ec.eu/BusinessMessages/TATAFng/Monitoring/V1">
+         |      <soap:Header>
+         |        <Action xmlns="http://www.w3.org/2005/08/addressing">CCN2.Service.Customs.Default.CSRD.ReferenceDataExportReceiverCBS/IsAlive</Action>
+         |        <MessageID xmlns="http://www.w3.org/2005/08/addressing">urn:uuid:fcb0896f-33d1-4542-8f64-1dce8101ca09</MessageID>
+         |      </soap:Header>
+         |      <soap:Body>
+         |        <v1:isAliveReqMsg />
+         |      </soap:Body>
+         |    </soap:Envelope>""".stripMargin
+
+  def xmlFullMessageIsAliveWithInvalidMessage: String =
+    s"""<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
+       |     xmlns:v1="http://xmlns.ec.eu/BusinessMessages/TATAFng/Monitoring/V1">
+       |      <soap:Header>
+       |        <Action xmlns="http://www.w3.org/2005/08/addressing">CCN2.Service.Customs.Default.CSRD.ReferenceDataExportReceiverCBS/IsAlive</Action>
+       |        <MessageID xmlns="http://www.w3.org/2005/08/addressing">urn:uuid:fcb0896f-33d1-4542-8f64-1dce8101ca09</MessageID>
+       |      </soap:Header>
+       |      <soap:Body>
+       |        <v1:isAliveReqMsg>
+       |                  <Should>
+       |                  <Not>
+       |                  <Be>
+       |                  <Present/>
+       |                  </Be>
+       |                  </Not>
+       |                  </Should>
+       |        </v1:isAliveReqMsg>
+       |      </soap:Body>
+       |    </soap:Envelope>""".stripMargin
 }
